6495386c9a9436cff0d51282b899ae6190f7b095
Kingdom3981 <kingdom3981rok@outlook.com>
2026-09-21 12:00:53 +0300

    Delete assets directory

--- assets/js/pwa.js
+++
@@ -1,8 +0,0 @@
-export function registerPwa() {
-  if (!("serviceWorker" in navigator)) return;
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js").catch(() => {
-      // The site remains usable if service worker registration is unavailable.
-    });
-  });
-}
